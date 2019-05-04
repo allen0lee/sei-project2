3c3bc606c36fc9c9f60c2dccfed00937026bdca4
Alan <allen0lee@gmail.com>
2019-05-05 03:52:24 +1000

    delete photo function

--- photo_sharing_app/main.rb
+++ photo_sharing_app/main.rb
@@ -10,7 +10,6 @@ require_relative 'models/album.rb'
 require_relative 'models/like.rb'
 enable :sessions
 
-
 helpers do
   def current_user
     User.find_by(id: session[:user_id])
@@ -21,12 +20,10 @@ helpers do
   end
 end
 
-
 # index page
 get '/' do       
   erb :index
 end
-
 
 # login page
 get '/login' do
@@ -48,13 +45,11 @@ delete '/session' do
   redirect '/'
 end
 
-
 # page that lists all users' albums - albums.erb
 get '/albums' do
   @albums = Album.all
   erb :albums
 end
-
 
 # page that lists one user's albums
 get '/albums/:user_id' do
@@ -63,7 +58,6 @@ get '/albums/:user_id' do
   erb :one_user_albums
 end
 
-
 # in user's own album page (or index after login), click 'create album' button, 
 # go to create_album page - create_new_album.erb
 get '/albums/:user_id/new' do
@@ -71,7 +65,7 @@ get '/albums/:user_id/new' do
   @user_id = params[:user_id]
   erb :create_new_album
 end
-# create new album here
+# create new album
 post '/albums/:user_id' do         
   album = Album.new
   album.name = params[:name]         # params[] - get value from a route or get user input 
@@ -80,7 +74,6 @@ post '/albums/:user_id' do
   album.save
   redirect "/albums/#{album.user_id}"
 end
-
 
 # show all photos of an album, with 'upload photo' button here - take user to upload_photo.erb
 get '/photos/:album_name/:album_id' do
@@ -93,7 +86,6 @@ get '/photos/:album_name/:album_id' do
   @user_id = session[:user_id]     
   erb :photos
 end
-
 
 # go to upload photo page
 get '/photos/:album_id/:user_id/new' do
@@ -116,7 +108,6 @@ post '/photos/:album_id/:user_id' do
   # remove space in album name, otherwise route not known by browser
   redirect "/photos/#{(album.name).split(" ").join("")}/#{photo.album_id}"
 end
-
 
 # show single photo, likes and comments - create comments, add likes here
 get '/photos/:id' do
@@ -157,14 +148,13 @@ put '/likes' do
   redirect "/photos/#{like.photo_id}"
 end
 
-
 # edit a photo
 get '/photos/:id/:album_id/edit' do
   redirect '/login' if !logged_in? # if not logged in, cannot edit photo
   @photo = Photo.find(params[:id])
   erb :edit_photo
 end
-# update a photo here
+# update a photo 
 put '/photos/:id' do
   photo = Photo.find(params[:id])
   photo.name = params[:name]
@@ -172,10 +162,13 @@ put '/photos/:id' do
   photo.save
   redirect "/photos/#{photo.id}"
 end
-
-
 # delete a photo
-
+delete '/photos/:id' do
+  photo = Photo.find(params[:id])
+  album = Album.where(id: photo.album_id).first
+  photo.destroy
+  redirect "/photos/#{(album.name).split(" ").join("")}/#{album.id}"
+end
 
 # delete an album
 
