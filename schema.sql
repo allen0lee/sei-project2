3c155b8256c1f38de531c5a9a892e196db1913ec
Alan <allen0lee@gmail.com>
2019-05-10 13:06:30 +1000

    change db sql schema and change some css

--- schema.sql
+++ schema.sql
@@ -2,37 +2,47 @@ create database photo_sharing_app;
 
 create table photos(
     id SERIAL PRIMARY KEY,
-    name TEXT,
+    name TEXT NOT NULL,
     image_url TEXT,
+    image_file_name TEXT,
     user_id INTEGER,
-    album_id INTEGER
+    album_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (album_id) REFERENCES albums (id) ON DELETE CASCADE
 );
 
 create table users(
     id SERIAL PRIMARY KEY,
-    email VARCHAR(500),
-    password_digest VARCHAR(500)
+    email VARCHAR(500) NOT NULL,
+    password_digest VARCHAR(500) NOT NULL
 );
 
 create table comments(
     id SERIAL PRIMARY KEY,
-    content TEXT,
+    content TEXT NOT NULL,
+    time TEXT,
     photo_id INTEGER,
     album_id INTEGER,
     user_id INTEGER,
-    time TEXT
+    FOREIGN KEY (photo_id) REFERENCES photos (id) ON DELETE CASCADE,
+    FOREIGN KEY (album_id) REFERENCES albums (id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 create table albums(
     id SERIAL PRIMARY KEY,
-    name TEXT,
+    name TEXT NOT NULL,
     theme_image_url TEXT,
-    user_id INTEGER
+    theme_image_file_name TEXT,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 create table likes(
     id SERIAL PRIMARY KEY,
     number INTEGER,
     photo_id INTEGER,
-    album_id INTEGER
+    album_id INTEGER,
+    FOREIGN KEY (photo_id) REFERENCES photos (id) ON DELETE CASCADE,
+    FOREIGN KEY (album_id) REFERENCES albums (id) ON DELETE CASCADE
 );
